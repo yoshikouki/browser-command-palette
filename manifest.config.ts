ada9733bcfe40b446c52937e0711f9eb853969f1
yoshikouki <yoshikouki@pepabo.com>
2025-12-23 22:55:13 +0900

    Update Logo

--- manifest.config.ts
+++ manifest.config.ts
@@ -6,11 +6,11 @@ export default defineManifest({
   name: pkg.name,
   version: pkg.version,
   icons: {
-    48: "public/logo.png",
+    24: "public/logo.png",
   },
   action: {
     default_icon: {
-      48: "public/logo.png",
+      24: "public/logo.png",
     },
     default_popup: "src/popup/index.html",
   },
